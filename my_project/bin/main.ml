85c9d3cba3f7e35b05b89cca9577d234a30cb9eb
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-19 10:21:02 -0500

    progress

--- my_project/bin/main.ml
+++ my_project/bin/main.ml
@@ -1,4 +1,8 @@
-open My_project.My_math
+(* To allow referring to the functions in a library file
+   without a library name prefix, open the library.
+   For example,open My_project.Math_lib *)
+open My_project
+open Printf
 
 (* At the module level, everthing needs to
    be in a binding created with `let`.
@@ -8,7 +12,5 @@ open My_project.My_math
    that the expression does not return a value. *)
 let () =
   let a = 1 and b = 2 and c = 3 in
-  print_int (add a b);
-  print_newline ();
-  print_float (average b c);
-  print_newline ()
+  printf "%d %d\n" (Math_local.add a b) (Math_lib.add a b);
+  printf "%f %f\n" (Math_local.average b c) (Math_lib.average b c)
---
+++ my_project/lib/math_lib.ml
@@ -0,0 +1,6 @@
+let add a b = a + b
+
+let average a b = float_of_int (add a b) /. 2.0
+
+(* let test "add" = MyMath.add 1 2 = 4 *)
+(* let test "average" = average 2 3 = 2.5 *)
