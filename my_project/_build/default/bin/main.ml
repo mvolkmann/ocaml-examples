0160b7b03beddfe724b13edafdf788560abde9c7
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-19 09:33:48 -0500

    progress

--- my_project/_build/default/bin/main.ml
+++
@@ -1,8 +0,0 @@
-open My_lib.My_math
-
-let () =
-  let a = 1 and b = 2 and c = 3 in
-  print_int (MyMath.add a b);
-  print_newline ();
-  print_float (MyMath.average b c);
-  print_newline ()
--- my_project/_build/default/lib/my_math.ml
+++
@@ -1,8 +0,0 @@
-module MyMath = struct
-  let add a b = a + b
-
-  let average a b = float_of_int (add a b) /. 2.0
-end
-
-(* let test "add" = MyMath.add 1 2 = 4 *)
-(* let test "average" = average 2 3 = 2.5 *)
