8e95e209bcf4eefa714d971fe9449b5a3885021e
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-19 09:33:58 -0500

    progress

--- my_project/bin/main.ml
+++ my_project/bin/main.ml
@@ -1,5 +1,11 @@
-open My_lib.My_math
+open My_project.My_math
 
+(* At the module level, everthing needs to
+   be in a binding created with `let`.
+   You could use `let _ =` which allows the
+   expression to have any kind of value.
+   But using `let () =` is preferred because it states
+   that the expression does not return a value. *)
 let () =
   let a = 1 and b = 2 and c = 3 in
   print_int (MyMath.add a b);
