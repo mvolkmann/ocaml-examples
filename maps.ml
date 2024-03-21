f4a2a31a762591942313486b55541557f97647ee
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-21 06:26:40 -0500

    progress

--- maps.ml
+++ maps.ml
@@ -1,23 +1,12 @@
 module StringMap = Map.Make (String)
 
-(* let () =
-  let map = StringMap.empty in
-  let map = map |> StringMap.add "a" "apple" in
-  let map = map |> StringMap.add "b" "banana" in
-  let map = map |> StringMap.update "a" (Option.map (fun _ -> "apricot")) in
-  let map = map |> StringMap.remove "b" in
+let () =
+  let map =
+    StringMap.empty |> StringMap.add "a" "apple" |> StringMap.add "b" "banana"
+    |> StringMap.update "a" (Option.map (fun _ -> "apricot"))
+    |> StringMap.remove "b"
+  in
   let a_fruit = StringMap.find_opt "a" map in
   match a_fruit with
   | None -> print_endline "No such fruit"
-  | Some fruit -> print_endline fruit *)
-
-let () =
-   let map = StringMap.empty in
-   |> StringMap.add "a" "apple"
-   |> StringMap.add "b" "banana"
-   |> StringMap.update "a" (Option.map (fun _ -> "apricot"))
-   |> StringMap.remove "b";
-   let a_fruit = StringMap.find_opt "a" map in
-   match a_fruit with
-   | None -> print_endline "No such fruit"
-   | Some fruit -> print_endline fruit
+  | Some fruit -> print_endline fruit
