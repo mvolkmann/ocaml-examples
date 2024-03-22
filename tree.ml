698edaad3006e174829e437af5aff2b65002e34b
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-22 10:08:16 -0500

    progress

--- tree.ml
+++ tree.ml
@@ -1,15 +1,20 @@
 open Printf
 
+(* A tree can be empty or it can have a node
+   that holds a left tree, a value, and a right tree. *)
 type 'a tree = Empty | Node of 'a tree * 'a * 'a tree
 
 let rec depth_first_in_order tree =
   match tree with
-  | Empty -> ()
+  | Empty -> () (* do nothing *)
   | Node (left, value, right) ->
       depth_first_in_order left;
       print_endline value;
       depth_first_in_order right
 
+(* This holds names in a tree so that when
+   printed with depth_first_in_order
+   they will appear in sorted order. *)
 let family_tree : string tree =
   Node
     ( Node (Empty, "Amanda", Empty),
