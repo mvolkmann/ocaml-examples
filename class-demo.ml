5a9ac522206790d3e06047c2cf3d732a86182e14
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-27 10:30:07 -0500

    progress

--- class-demo.ml
+++ class-demo.ml
@@ -3,6 +3,11 @@ open Printf
 (* Classes offer an alternative to records.
    While both can define fields,
    classes add the ability to define methods. *)
+
+(* This defines both a class and its single constructor.
+   Constructor parameters, fields, and method parmeters
+   can have generic types (ex. 'a').
+*)
 class point (x_init : float) (y_init : float) =
   (* Can omit "(self)" if there are no references to it. *)
   object (self)
@@ -23,10 +28,24 @@ class point (x_init : float) (y_init : float) =
   end
 
 let () =
+  (* Use the new keyword to create an instance of the class. *)
   let p = new point 0.0 0.0 in
+
   (* Methods are called with # instead of dot. *)
   p#set_x 1.0;
   p#set_y 2.0;
   p#translate 3.0 4.0;
   printf "(%f, %f)\n" p#get_x p#get_y;
-  p#print (* No need to pass `()`. *)
+  p#print
+(* No need to pass `()`. *)
+
+(*
+Abstract classes are defined with "class virtual".
+Methods can be defined with "method virtual"
+to require subclasses to implement them.
+In inherit from an abstract class, add "inherit {class_name} {args}"
+inside "object (self)" to call its constructor.
+To enable calling superclass methods,
+add "inherit {class_name} {args} as super"
+and then use "super#{method_name}" to call them.
+*)
