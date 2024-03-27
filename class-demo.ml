d7beccd83df975bbaa0eeff79448df6a5bc97c37
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-27 10:48:30 -0500

    progress

--- class-demo.ml
+++ class-demo.ml
@@ -11,7 +11,8 @@ open Printf
 class point (x_init : float) (y_init : float) =
   (* Can omit "(self)" if there are no references to it. *)
   object (self)
-    (* instance variables *)
+    (* Instance variables are immutable by default,
+       but can be made mutable. *)
     val mutable x = x_init
     val mutable y = y_init
 
@@ -27,6 +28,44 @@ class point (x_init : float) (y_init : float) =
       y <- y +. dy
   end
 
+(*
+Abstract classes are defined with "class virtual".
+Methods can be defined with "method virtual"
+to require subclasses to implement them.
+In inherit from an abstract class, add "inherit {class_name} {args}"
+inside "object (self)" to call its constructor.
+Arguments passed to a constructor do not require a matching field
+and can be used in methods.
+To enable calling superclass methods,
+add "inherit {class_name} {args} as super"
+and then use "super#{method_name}" to call them.
+To coerce a subclass value to a superclass type, `obj :> {superclass}`.
+*)
+
+class virtual shape (name_init : string) =
+  object
+    val name = name_init
+    method virtual area : float
+  end
+
+class circle (center_init : point) (radius_init : float) =
+  object
+    inherit shape "circle"
+    val center = center_init
+    val radius = radius_init
+    method area = 3.14159 *. radius *. radius
+  end
+
+class rectangle (lower_left_init : point) (width_init : float)
+  (height_init : float) =
+  object
+    inherit shape "rectangle"
+    val lower_left = lower_left_init
+    val width = width_init
+    val height = height_init
+    method area = width *. height
+  end
+
 let () =
   (* Use the new keyword to create an instance of the class. *)
   let p = new point 0.0 0.0 in
@@ -36,16 +75,19 @@ let () =
   p#set_y 2.0;
   p#translate 3.0 4.0;
   printf "(%f, %f)\n" p#get_x p#get_y;
-  p#print
-(* No need to pass `()`. *)
+  (* There is no need to pass the unit value `()`
+     to call methods that have no parameters. *)
+  p#print;
+  (* The Oo.copy function makes a shallow copy of an object. *)
+  let p2 = Oo.copy p in
+  p2#set_x 5.0;
+  p2#print;
+  (* Each object is assigned a unique id
+     that can be accessed with Oo.id function. *)
+  printf "p id = %d\n" (Oo.id p);
+  printf "p2 id = %d\n" (Oo.id p2);
 
-(*
-Abstract classes are defined with "class virtual".
-Methods can be defined with "method virtual"
-to require subclasses to implement them.
-In inherit from an abstract class, add "inherit {class_name} {args}"
-inside "object (self)" to call its constructor.
-To enable calling superclass methods,
-add "inherit {class_name} {args} as super"
-and then use "super#{method_name}" to call them.
-*)
+  let c = new circle p 5.0 in
+  printf "circle area = %f\n" c#area;
+  let r = new rectangle p 10.0 5.0 in
+  printf "rectangle area = %f\n" r#area
