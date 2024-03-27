1ccf93dfc41cb3918338d98fb2e2a4b762d37cfb
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-27 11:43:13 -0500

    progress

--- class-demo.ml
+++ class-demo.ml
@@ -1,13 +1,5 @@
 open Printf
 
-(* Classes offer an alternative to records.
-   While both can define fields,
-   classes add the ability to define methods. *)
-
-(* This defines both a class and its single constructor.
-   Constructor parameters, fields, and method parmeters
-   can have generic types (ex. 'a').
-*)
 class point (x_init : float) (y_init : float) =
   (* Can omit "(self)" if there are no references to it. *)
   object (self)
@@ -16,54 +8,19 @@ class point (x_init : float) (y_init : float) =
     val mutable x = x_init
     val mutable y = y_init
 
-    (* methods *)
-    method get_x = x
-    method get_y = y
+    (* Getter methods can have the same name as the field they return. *)
+    method x = x
+    method y = y
     method set_x new_x = x <- new_x
     method set_y new_y = y <- new_y
+
+    (* This is a named constructor. *)
+    method origin = new point 0.0 0.0
     method print = printf "(%f, %f)\n" x y
 
     method translate dx dy =
       x <- x +. dx;
       y <- y +. dy
-  end
-
-(*
-Abstract classes are defined with "class virtual".
-Methods can be defined with "method virtual"
-to require subclasses to implement them.
-In inherit from an abstract class, add "inherit {class_name} {args}"
-inside "object (self)" to call its constructor.
-Arguments passed to a constructor do not require a matching field
-and can be used in methods.
-To enable calling superclass methods,
-add "inherit {class_name} {args} as super"
-and then use "super#{method_name}" to call them.
-To coerce a subclass value to a superclass type, `obj :> {superclass}`.
-*)
-
-class virtual shape (name_init : string) =
-  object
-    val name = name_init
-    method virtual area : float
-  end
-
-class circle (center_init : point) (radius_init : float) =
-  object
-    inherit shape "circle"
-    val center = center_init
-    val radius = radius_init
-    method area = 3.14159 *. radius *. radius
-  end
-
-class rectangle (lower_left_init : point) (width_init : float)
-  (height_init : float) =
-  object
-    inherit shape "rectangle"
-    val lower_left = lower_left_init
-    val width = width_init
-    val height = height_init
-    method area = width *. height
   end
 
 let () =
@@ -74,7 +31,7 @@ let () =
   p#set_x 1.0;
   p#set_y 2.0;
   p#translate 3.0 4.0;
-  printf "(%f, %f)\n" p#get_x p#get_y;
+  printf "(%f, %f)\n" p#x p#y;
   (* There is no need to pass the unit value `()`
      to call methods that have no parameters. *)
   p#print;
@@ -85,9 +42,4 @@ let () =
   (* Each object is assigned a unique id
      that can be accessed with Oo.id function. *)
   printf "p id = %d\n" (Oo.id p);
-  printf "p2 id = %d\n" (Oo.id p2);
-
-  let c = new circle p 5.0 in
-  printf "circle area = %f\n" c#area;
-  let r = new rectangle p 10.0 5.0 in
-  printf "rectangle area = %f\n" r#area
+  printf "p2 id = %d\n" (Oo.id p2)
---
+++ class-demo2.ml
@@ -0,0 +1,49 @@
+open Printf
+
+class point (x_init : float) (y_init : float) =
+  object (self)
+    val mutable x = x_init
+    val mutable y = y_init
+    method get_x = x
+    method get_y = y
+    method set_x new_x = x <- new_x
+    method set_y new_y = y <- new_y
+    method origin = new point 0.0 0.0
+    method print = printf "(%f, %f)\n" x y
+
+    method translate dx dy =
+      x <- x +. dx;
+      y <- y +. dy
+  end
+
+class virtual shape (name_init : string) =
+  object
+    val name = name_init
+    method name = name
+    method virtual area : float
+  end
+
+class circle (center_init : point) (radius_init : float) =
+  object
+    inherit shape "circle"
+    val center = center_init
+    val radius = radius_init
+    method area = 3.14159 *. radius *. radius
+  end
+
+class rectangle (lower_left_init : point) (width_init : float)
+  (height_init : float) =
+  object
+    inherit shape "rectangle"
+    val lower_left = lower_left_init
+    val width = width_init
+    val height = height_init
+    method area = width *. height
+  end
+
+let () =
+  let p = new point 0.0 0.0 in
+  let c = new circle p 5.0 in
+  printf "%s area = %f\n" c#name c#area;
+  let r = new rectangle p 10.0 5.0 in
+  printf "%s area = %f\n" r#name r#area
