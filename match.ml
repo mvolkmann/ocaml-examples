fd5fffb7e636df7d1100af9992c273779f52fc11
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-03-29 14:05:54 -0500

    progress

--- match.ml
+++ match.ml
@@ -1,13 +1,27 @@
-open Random
+type season = Winter | Spring | Summer | Fall
+
+let weather s =
+  match s with
+  | Winter -> "cold"
+  | Spring -> "cool"
+  | Summer -> "hot"
+  | Fall -> "perfect"
+
+let weather2 = function
+  | Winter -> "cold"
+  | Spring -> "cool"
+  | Summer -> "hot"
+  | Fall -> "perfect"
 
 let () =
+  print_endline (weather Fall);
+  print_endline (weather2 Fall);
+
   Random.self_init ();
   let n = Random.int 10 in
   print_int n;
   print_newline ();
   match n with
-  | n when n < 3 ->
-      print_string "test";
-      print_string "small"
-  | n when 4 <= n && n <= 7 -> print_string "medium"
-  | _ -> print_string "large"
+  | n when n < 3 -> print_endline "small"
+  | n when 4 <= n && n <= 7 -> print_endline "medium"
+  | _ -> print_endline "large"
