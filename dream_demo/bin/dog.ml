1ef35c0714c3e8eea545a1e97607adb2a5589e9d
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-05 16:42:02 -0500

    added unit tests in dream_demo project

--- dream_demo/bin/dog.ml
+++ dream_demo/bin/dog.ml
@@ -4,3 +4,14 @@ type t = { id : string; name : string; breed : string } [@@deriving yojson]
 
 let breed_of_opt = Option.fold ~some:(fun dog -> dog.breed) ~none:""
 let name_of_opt = Option.fold ~some:(fun dog -> dog.name) ~none:""
+
+let%test "test breed_of_opt with some" =
+  breed_of_opt (Some { id = "1"; name = "Buddy"; breed = "Labrador" })
+  = "Labrador"
+
+let%test "test breed_of_opt with none" = breed_of_opt None = ""
+
+let%test "test name_of_opt with some" =
+  name_of_opt (Some { id = "1"; name = "Buddy"; breed = "Labrador" }) = "Buddy"
+
+let%test "test name_of_opt with none" = name_of_opt None = ""
