1faf94e7b89bc07096b51a5672edb41f552441bb
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-09 12:41:39 -0500

    progress

--- base_demo/bin/main.ml
+++ base_demo/bin/main.ml
@@ -1,6 +1,13 @@
 open Base
 open Printf
 
+(* From RegularSpatula
+   Base has int int exponent, float ^ int exponent (fast),
+   and float ^ float exponentiation.
+   val int_pow : float -> int -> float (in Float module)
+   They say it is 5-10x faster than `**.` for exponent up to ~1000*.
+*)
+
 let () =
   (* printf "integer exponentiation = %d\n" 2 ** 3; *)
   (* printf "float exponentiation = %f\n" (2.3 ** 4.5) *)
