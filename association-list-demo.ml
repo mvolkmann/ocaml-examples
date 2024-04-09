ce1b11957c5a86fbbd0246671f9758dfc74ed083
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-09 10:20:12 -0500

    progress

--- association-list-demo.ml
+++ association-list-demo.ml
@@ -1,11 +1,20 @@
-open Printf
-
+(* Create an association list. *)
 let al1 = [ ("red", "FF0000"); ("green", "00FF00"); ("blue", "0000FF") ]
+
+(* Create a new association list from al1
+   where the "green" key/value pair is removed. *)
 let al2 = List.remove_assoc "green" al1
+
+(* Create a new association list from al1
+   where the "yellow" key/value pair is added. *)
 let al3 = ("yellow", "FFFF00") :: al2
 
 let () =
+  (* Get the value for the "red" key in al1. *)
   assert (List.assoc "red" al1 = "FF0000");
+  (* Determine if the key "red" is present in al1. *)
   assert (List.mem_assoc "red" al1);
+  (* Determine if the key "purple" is present in al1. *)
   assert (not (List.mem_assoc "purple" al1));
+  (* Determine if the key "yellow" is present in al3. *)
   assert (List.mem_assoc "yellow" al3)
--- tuples.ml
+++ tuples.ml
@@ -3,14 +3,21 @@ open Printf
 (* Get the second element of a 4-element tuple using destructuring. *)
 let second (_, e, _, _) = e
 
-(* Get the third element of a 4-element tuple using match. *)
-let third tuple = match tuple with a, b, c, d -> c
+(* This shows three ways to write a function that
+   returns the third elemet of a 4-element tuple `t`. *)
+let third_of_4 t = match t with a, b, c, d -> c
+
+(* let third_of_4 t =
+   let _, _, c, _ = t in
+   c *)
+
+let third_of_4 (_, _, v, _) = v
 let greek = ("alpha", "beta", "gamma", "delta")
 
 let () =
   print_endline (second greek);
   (* beta *)
-  print_endline (third greek);
+  print_endline (third_of_4 greek);
 
   (* gamma *)
   let t = (true, 3, "blue") in
