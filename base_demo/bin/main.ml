f5cf4f27fc1849be40d59dee6bf8bbf8e321d1cf
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-10 11:03:35 -0500

    progress

--- base_demo/bin/main.ml
+++ base_demo/bin/main.ml
@@ -12,6 +12,8 @@ let square x = x * x
 (* The functions are applied from right to left. *)
 let add1square = Fn.compose square add1
 
+let my_list = [1; 2; 3; 4; 5]
+
 let () =
   (* printf "integer exponentiation = %d\n" 2 ** 3; *)
   (* printf "float exponentiation = %f\n" (2.3 ** 4.5) *)
@@ -19,4 +21,15 @@ let () =
   let f = Int.to_float i in
   Stdlib.print_float (f *. 3.0);
 
-  assert (add1square 2 = 9)
+  assert (add1square 2 = 9);
+
+  (* For warnings about using polymorphic comparisons, see 
+     https://blog.janestreet.com/the-perils-of-polymorphic-compare/
+     and https://blog.janestreet.com/building-a-better-compare/ *)
+  let front = List.take my_list 3 in
+  assert Poly.(front = [1; 2; 3]);
+  assert (List.equal Int.equal front [1; 2; 3]);
+
+  let back = List.drop my_list 3 in
+  assert Poly.(back = [4; 5]);
+  assert (List.equal Int.equal back [4; 5]);
