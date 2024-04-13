ac8c4f4a3aa2b7245d5f3ad235fe59681adef093
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-13 16:55:55 -0500

    progress

--- module_demo2/lib/math.mli
+++ module_demo2/lib/math.mli
@@ -1,3 +1,9 @@
 type point = float * float
 
+(**
+Computes the distance from one point to another.
+@param p1 the first point
+@param p2 the second point
+@return the distance between them
+*)
 val distance : point -> point -> float
