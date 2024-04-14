9263eef1294407bb20ad9c88aec3f5993a5d8bca
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-13 19:01:43 -0500

    progress

--- samples/time_zone.ml
+++ samples/time_zone.ml
@@ -1,11 +1,22 @@
 open Core
-open Core_unix
+
+let readline () = In_channel.input_line In_channel.stdin
 
 let () =
-  printf "Enter a timezone: ";
-  match In_channel.input_line In_channel.stdin with
+  (* Is there another function I could use that would not
+     require flushing the buffer with %! ? *)
+  printf "Enter a timezone: %!";
+  (* Enter a value like "America/Chicago" or "Europe/London". *)
+  match readline () with
+  (* Pressing return without entering text gives Some "", not None. *)
   | None -> failwith "No timezone was provided"
   | Some zone_string ->
-      let zone = Time.Zone.find_exn zone_string in
-      let time_string = time.to_string_abs (Time.now ()) ~zone in
-      printf "The time in %s is %s.\n%!" (Time.Zone.to_string zone) time_string
+      let open Time_float_unix in
+      let now = Time_float.now () in
+      (* Can I make this work with timezone abbreviations
+         like CST instead of long timezone names? *)
+      let zone = Zone.find_exn zone_string in
+      (* How can I get only the time without
+         the date, seconds, and timezone offset? *)
+      let time_string = to_string_abs now ~zone in
+      printf "The time in %s is %s.\n" (Zone.to_string zone) time_string
