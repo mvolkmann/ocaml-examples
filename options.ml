7154b9e2001d891935a0203b06c8c98c7fc73c3a
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-15 10:55:30 -0500

    progress

--- options.ml
+++ options.ml
@@ -1,9 +1,21 @@
+let options = [ Some 1; None; Some 2 ]
 let double x = x * 2
 let is_even x = x mod 2 == 0
 
 let () =
+  List.iter
+    (fun x ->
+      match x with
+      | None -> print_endline "none"
+      | Some n ->
+          print_int n;
+          print_newline ())
+    options;
+
   let numbers = [ 1; 4; 7 ] in
+  (* Find the first even number, double it, and print it. *)
   List.find_opt is_even numbers
-  |> Option.map double |> Option.map string_of_int
+  |> Option.map double
+  |> Option.map string_of_int
   |> Option.value ~default:"no even found"
   |> print_endline
