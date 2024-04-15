6dfe4b74cc03e6b1a78fa9624f499a700bb2f539
Mark Volkmann <r.mark.volkmann@gmail.com>
2024-04-15 09:01:09 -0500

    progress

--- default-parameters.ml
+++ default-parameters.ml
@@ -2,17 +2,16 @@ open Printf
 
 let product ?(a = 1.0) b = a *. b
 
-(* When using optional parameters, there must be at least one
-   that is not optional.  Adding `()` satisifies this. *)
-let greet ?(name = "World") ?(suffix = "!") () =
-  printf "Hello, %s%s\n" name suffix
+(* When using optional parameters, there must be at least one that is not
+   optional. Adding `()` satisifies this. *)
+let greet ?(name = "World") ?suffix:(s = "!") () = printf "Hello, %s%s\n" name s
 
 let () =
   printf "product = %f\n" (product ~a:8.5 2.0);
   printf "product = %f\n" (product 8.5);
 
-  (* In calls to functions with no required parameters, include `()`.
-     It doesn't matter where it appears in the argument list. *)
+  (* In calls to functions with no required parameters, include `()`. It doesn't
+     matter where it appears in the argument list. *)
   greet ~name:"Mark" ~suffix:"." ();
-  greet () ~name:"Mark";
+  greet ~name:"Mark" ();
   greet ()
